e722e9f03e6c4d105e951ea736c07f8366207b64
RATIONAL331 <rational331@gmail.com>
2021-01-18 20:57:57 +0900

    first commit

---
+++ src/test/java/nhn/rookieHAMATF/ryungjinkim_guestbook/RyungjinkimGuestbookApplicationTests.java
@@ -0,0 +1,13 @@
+package nhn.rookieHAMATF.ryungjinkim_guestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyungjinkimGuestbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
