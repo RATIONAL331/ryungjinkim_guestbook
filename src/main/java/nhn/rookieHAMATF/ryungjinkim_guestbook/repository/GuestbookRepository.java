10aa2bb5cdb24a469c5d3795596a135f02bd9b78
RyungJinKim <rational331@gmail.com>
2021-01-19 12:27:30 +0900

    querydsl finish

--- src/main/java/nhn/rookieHAMATF/ryungjinkim_guestbook/repository/GuestbookRepository.java
+++ src/main/java/nhn/rookieHAMATF/ryungjinkim_guestbook/repository/GuestbookRepository.java
@@ -4,4 +4,5 @@ import nhn.rookieHAMATF.ryungjinkim_guestbook.entity.Guestbook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+
 }
